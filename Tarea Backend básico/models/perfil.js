d9c7546cd8010bdab7023f08683afc69d80bc436
elalextx <alexcodigohanke@gmail.com>
2025-11-03 20:58:37 -0300

    Delete Tarea Backend básico directory

--- Tarea Backend básico/models/perfil.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const perfilSchema = mongoose.Schema({
-    nombre: String
-});
-
-module.exports = mongoose.model('Perfil', perfilSchema);
--- Tarea Backend básico/models/usuario.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const usuarioSchema = mongoose.Schema({
-    nombre: String,
-    pass: String,
-    perfil: { type: mongoose.Schema.Types.ObjectId, ref: 'Perfil' }
-
-});
-
-module.exports = mongoose.model('Usuario', usuarioSchema);
