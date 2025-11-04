625c1cff35c18a6a783983c8e1d49d9da0309833
elalextx <alexcodigohanke@gmail.com>
2025-11-03 23:17:07 -0300

    Delete models directory

--- models/cliente.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const clienteSchema = new mongoose.Schema({
-    historialCompras: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Compra' }],
-    estado: { type: String, default: 'pendiente' }
-});
-
-module.exports = mongoose.model('Cliente', clienteSchema);
